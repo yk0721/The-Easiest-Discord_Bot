a5323707a3e5d0eb300858566f56f75d764b5447
yk0721 <dredgk0@gmail.com>
2024-12-01 14:13:16 +0900

    added ini file support.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 #Discord-bot-by-yourself...
 
 import discord
+import configparser
 
-Token = "MTMxMjYzNDkyMDQwOTEwNDQ3NQ.Gw8rth.3thEAnqN7vkQkdc6z07f7hcov0i5PQ00lJNkFQ"
+#load config.ini file
+config_ini = configparser.ConfigParser()
+config_ini.read('config.ini')
+#get config_data
+Token = config_ini['General']['Token']
+
+#initialize discord_client
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
 
+#event on ready
 @client.event
 async def on_ready():
     print(f'{client.user} でログインしました。')
@@ -18,5 +25,5 @@ async def on_message(message):
         return
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-
+#run client
 client.run(Token)
